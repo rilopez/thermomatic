14afb497cbb735158af12cf37d08baa5f471127c
Isaac Lopez <ilopez@nearsoft.com>
2020-08-01 12:16:15 -0500

    internal/imei: `Decode` converts a byte array IMEI to uint64 decimal IMEI When devices connect to our servers they send a 15-byte long message containing their IMEI code in decimal format , this commit introduced a utility function to convert & validate that message to an int64 Fixes: #1

--- internal/imei/imei.go
+++ internal/imei/imei.go
@@ -8,8 +8,7 @@ package imei
 
 import (
 	"errors"
-
-	"github.com/spin-org/thermomatic/internal/common"
+	"math"
 )
 
 var (
@@ -27,5 +26,34 @@ var (
 // Decode does NOT allocate under any condition. Additionally, it panics if b
 // isn't at least 15 bytes long.
 func Decode(b []byte) (code uint64, err error) {
-	panic(common.ErrNotImplemented)
+	_ = b[14] // nice trick to hint bound checks to the compiler | https://medium.com/@brianblakewong/optimizing-go-bounds-check-elimination-f4be681ba030
+
+	place := 14
+	var checksum byte
+	var imeiAsFloat float64
+
+	for i := 0; i < 15; i++ {
+		currDigit := b[i]
+		digitForChecksum := currDigit
+		if currDigit > 9 {
+			// In case b isn't strictly composed of digits, the returned error will be
+			// ErrInvalid.
+			return 0, ErrInvalid
+		}
+		if (i+1)%2 == 0 {
+			digitForChecksum *= 2
+			if digitForChecksum > 9 {
+				digitForChecksum = (digitForChecksum % 10) + 1
+			}
+		}
+		checksum += digitForChecksum
+		imeiAsFloat += float64(currDigit) * math.Pow10(place)
+		place--
+	}
+
+	if checksum%10 != 0 {
+		return 0, ErrChecksum
+	}
+
+	return uint64(imeiAsFloat), nil
 }
--- internal/imei/imei_test.go
+++ internal/imei/imei_test.go
@@ -1,23 +1,69 @@
 package imei
 
 import (
+	"runtime"
 	"testing"
-
-	"github.com/spin-org/thermomatic/internal/common"
 )
 
 func TestDecode(t *testing.T) {
-	panic(common.ErrNotImplemented)
+	b := []byte{4, 9, 0, 1, 5, 4, 2, 0, 3, 2, 3, 7, 5, 1, 8}
+	expectedIMEI := uint64(490154203237518)
+	actualIMEI, _ := Decode(b)
+
+	if actualIMEI != expectedIMEI {
+		t.Errorf("expecting imei %d but got %d", expectedIMEI, actualIMEI)
+	}
+}
+
+func TestDecodeErrCheckSum(t *testing.T) {
+	b := []byte{4, 9, 0, 1, 5, 4, 2, 0, 3, 2, 3, 7, 5, 1, 1}
+	_, err := Decode(b)
+	if err != ErrChecksum {
+		t.Errorf("expecting ErrChecksum but got  %v", err)
+	}
+}
+
+func TestDecodeWithNonDigitsErrInvalid(t *testing.T) {
+	b := []byte{4, 9, 'A', 'B', 5, 4, 2, 0, 3, 2, 3, 7, 5, 1, 8}
+	_, err := Decode(b)
+	if err != ErrInvalid {
+		t.Errorf("expecting ErrInvalid but got %s", err)
+	}
+}
+
+func TestDecodePanicAtLeast15byteslong(t *testing.T) {
+	//it panics if b isn't at least 15 bytes long.
+	onlyTwoBytes := []byte{1, 2}
+
+	shouldPanic(t, func() {
+		_, _ = Decode(onlyTwoBytes)
+	})
 }
 
 func TestDecodeAllocations(t *testing.T) {
-	panic(common.ErrNotImplemented)
-}
-
-func TestDecodePanics(t *testing.T) {
-	panic(common.ErrNotImplemented)
+	defer runtime.GOMAXPROCS(runtime.GOMAXPROCS(1))
+	var start, end runtime.MemStats
+	runtime.GC()
+	runtime.ReadMemStats(&start)
+	_, _ = Decode([]byte{4, 9, 0, 1, 5, 4, 2, 0, 3, 2, 3, 7, 5, 1, 8})
+	runtime.ReadMemStats(&end)
+	alloc := end.TotalAlloc - start.TotalAlloc
+	if alloc > 0 {
+		t.Errorf("Decode should NOT allocate under any condition, it allocated %d bytes", alloc)
+	}
 }
 
 func BenchmarkDecode(b *testing.B) {
-	panic(common.ErrNotImplemented)
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = Decode([]byte{4, 9, 0, 1, 5, 4, 2, 0, 3, 2, 3, 7, 5, 1, 8})
+	}
+	b.StopTimer()
+}
+
+func shouldPanic(t *testing.T, f func()) {
+	defer func() { recover() }()
+	f()
+	t.Errorf("should have panicked")
 }
